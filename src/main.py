d2ebb588377156fe4a8763205b3b05062fb3ce52
Paweł <neotheprogramist@proton.me>
2023-10-01 09:35:07 +0200

    basic implementation

---
+++ src/main.py
@@ -0,0 +1,32 @@
+from secrets import choice, randbelow
+
+UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LOWER = "abcdefghijklmnopqrstuvwxyz"
+DIGITS = "0123456789"
+SPECIAL = "!@#$%^&*"
+
+
+def random_permutation(l: list):
+    l_len = len(l)
+    for i in range(l_len):
+        j = i + randbelow(l_len - i)
+        t = l[j]
+        l[j] = l[i]
+        l[i] = t
+    return l
+
+
+def main():
+    p = random_permutation([i for i in range(16)])
+    result = [None for _ in range(len(p))]
+    for i in range(0, 1):
+        result[p[i]] = choice(SPECIAL)
+    for i in range(1, 2):
+        result[p[i]] = choice(DIGITS)
+    for i in range(2, len(p)):
+        result[p[i]] = choice(UPPER + LOWER + DIGITS + SPECIAL)
+    print("".join(result))
+
+
+if __name__ == "__main__":
+    main()
